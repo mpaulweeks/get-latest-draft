c6a7625fc7d147728578d54bcab88fd97dbaed96
mpaulweeks <mpaulweeks@gmail.com>
2021-09-28 16:56:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async function run() {
+  try {
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    const myToken = core.getInput('token');
+
+    const context = github.context;
+    const octokit = github.getOctokit(myToken);
+    const response = await octokit.rest.repos.listReleases({
+      owner: context.owner,
+      repo: context.repo,
+    });
+
+    const drafts = response.data.filter(rel => rel.draft);
+    console.log(drafts);
+
+    const latest = drafts[0];
+    console.log(latest);
+    core.setOutput('id', latest.id.toString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
